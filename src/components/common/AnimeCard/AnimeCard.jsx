fd7b53285753dafd3a750cf2024f7a28f402efe1
Yves / Stan <48611390+stanlee17@users.noreply.github.com>
2023-06-05 23:49:36 +1000

    AnimeCard: row changed to 3 on mobile

--- src/components/common/AnimeCard/AnimeCard.jsx
+++ src/components/common/AnimeCard/AnimeCard.jsx
@@ -7,7 +7,7 @@ const AnimeCard = ({ data }) => {
   return (
     data && (
       <div>
-        <Row xxl={6} lg={6} md={3} xs={2} className="g-4">
+        <Row xxl={6} lg={6} md={3} xs={3} className="g-4">
           {data &&
             data.map((anime) => (
               <Col key={anime.mal_id}>
