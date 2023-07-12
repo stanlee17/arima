a2a98cb0cd5cd29fefa1f51731dddac8fcf42e27
Yves / Stan <48611390+stanlee17@users.noreply.github.com>
2023-07-12 23:45:12 +1000

    document type error fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" style={{ scrollBehavior: 'smooth' }}>
-      <Head
-        name="description"
-        content="Experience the ultimate anime info on Arima. Discover a vast collection of captivating anime series, stay updated on the latest releases, newly top-rated and upcoming Anime on Arima."
-        key="desc"
-      />
+      <Head>
+        <meta
+          name="description"
+          content="Experience the ultimate anime info on Arima. Discover a vast collection of captivating anime series, stay updated on the latest releases, newly top-rated and upcoming Anime on Arima."
+          key="desc"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
